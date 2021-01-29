343f1b3471d9206813ced995c875e46181f3223f
Li-Jinjie <lijinjie362@outlook.com>
2021-01-29 09:00:30 +0800

    add dropout and change the rate depending on training or testing

--- tf_network.py
+++ tf_network.py
@@ -12,7 +12,7 @@ class neuralNetwork:
 
         pass
 
-    def Net(self, input):
+    def Net(self, input, prob):
         '''
         Define network.
         You can use init_weight() and init_bias() function to init weight matrix,
@@ -49,55 +49,61 @@ class neuralNetwork:
         # N=(W-F+2P)/S+1
         # Layer 1: Convolutional. Input = 224x224x3. Output = 54x54x96.
         x = nn.conv2d(input, conv1_W, strides=[1, 4, 4, 1], padding=[[0, 0], [1, 1], [1, 1], [0, 0]]) + conv1_b
-        x = nn.relu(x)
+        x = nn.relu(x, name="conv_layer_01/relu")
 
         # Pooling. Input = 54x54x96. Output = 26x26x96.
         x = nn.max_pool2d(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
-                        padding='VALID')  # [batch, height, width, channels]
+                          padding='VALID', name="conv_layer_01/pooling")  # [batch, height, width, channels]
 
         # Layer 2: Convolutional. Input = 26x26x96. Output = 26x26x256.
         x = nn.conv2d(x, conv2_W, strides=[1, 1, 1, 1], padding='SAME') + conv2_b
-        x = nn.relu(x)
+        x = nn.relu(x, name="conv_layer_02/relu")
 
         # Pooling. Input = 26x26x256. Output = 12x12x256.
-        x = nn.max_pool2d(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
+        x = nn.max_pool2d(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name="conv_layer_02/pooling")
 
         # Layer 3: Convolutional. Input = 12x12x256. Output = 12x12x384.
         x = nn.conv2d(x, conv3_W, strides=[1, 1, 1, 1], padding='SAME') + conv3_b
-        x = nn.relu(x)
+        x = nn.relu(x, name="conv_layer_03/relu")
 
         # Layer 4: Convolutional. Input = 12x12x384. Output = 12x12x384.
         x = nn.conv2d(x, conv4_W, strides=[1, 1, 1, 1], padding='SAME') + conv4_b
-        x = nn.relu(x)
+        x = nn.relu(x, name="conv_layer_04/relu")
 
         # Layer 5: Convolutional. Input = 12x12x384. Output = 12x12x256.
         x = nn.conv2d(x, conv5_W, strides=[1, 1, 1, 1], padding='SAME') + conv5_b
-        x = nn.relu(x)
+        x = nn.relu(x, name="conv_layer_05/relu")
 
         # Pooling. Input = 12x12x256. Output = 5x5x256.
-        x = nn.max_pool2d(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
+        x = nn.max_pool2d(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name="conv_layer_05/pooling")
 
         # Layer 6: Fully Connected. Input = 5x5x256=6400. Output = 4096.
         x = flatten(x)
         x = tf.matmul(x, fc1_W) + fc1_b
-        x = nn.relu(x)
+        x = nn.relu(x, name="full_layer_01/relu")
+
+        # Dropout
+        x = nn.dropout(x, rate=prob)
 
         # Layer 7: Fully Connected. Input = 4096. Output = 4096.
         x = tf.matmul(x, fc2_W) + fc2_b
-        x = nn.relu(x)
+        x = nn.relu(x, name="full_layer_02/relu")
+
+        # Dropout
+        x = nn.dropout(x, rate=prob)
 
         # # Layer 8: Fully Connected. Input = 4096. Output = 10.
-        x = tf.matmul(x, fc3_W) + fc3_b
+        x = tf.add(tf.matmul(x, fc3_W), fc3_b, name="full_layer_03/linear")
 
-        logits = x
+        logits = x  # logits.shape = (batch_size, 10)
 
         return logits
 
-    def forward(self, input):
+    def forward(self, input, prob):
         '''
         Forward the network
         '''
-        return self.Net(input)
+        return self.Net(input, prob)
 
     def init_weight(self, shape):
         '''
